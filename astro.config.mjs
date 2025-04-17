27f58c0ec223cddf49b0bc963c480f325483226d
virajbhapkar6 <virajbhapkar6@gmail.com>
2025-04-17 14:49:28 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Required for API endpoint
+  output: 'server',
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [solidJs()]
+});
